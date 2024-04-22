5c87e382ade9820454cc848e8a37d536b6b7120e
Joseceron101 <2430933@clases.edu.sv>
2024-04-22 08:24:40 -0600

    Agregar archivos de proyecto.

---
+++ pawmart_jc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pawmart_jc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
